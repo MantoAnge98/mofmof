58c67865d9c7374ed50697e51ae39100f2dc6923
Ange <angepop1998@gmail.com>
2020-09-08 19:20:12 +0000

    First Commit

---
+++ app/models/nearest_station.rb
@@ -0,0 +1,8 @@
+class NearestStation < ApplicationRecord
+  belongs_to :property
+
+  validates :route_name, presence: true, on: :update
+  validates :station_name, presence: true, on: :update
+  validates :time, presence: true, on: :update
+
+end
